92bd6466256fe50bd4ed012a7211f78ff6ce77eb
isllas <86786511+isllas@users.noreply.github.com>
2024-05-13 11:41:36 -0300

    Coleta.py

--- Coleta.py
+++ Coleta.py
@@ -15,7 +15,7 @@ catraca_url = "IP de Acesso"
 chrome_options = webdriver.ChromeOptions()
 
 # Execução em modo headless (sem interface gráfica)
-chrome_options.add_argument("--headless")  
+#chrome_options.add_argument("--headless")   causa interagbilidade nos botões necessários
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
